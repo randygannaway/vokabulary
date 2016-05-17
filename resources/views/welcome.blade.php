d3e22471325001a2cf86cb0cfefb923fb03ed078
randygan <randygannaway@gmail.com>
2016-05-17 08:39:08 -0500

    changed welcome and colors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,15 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Welcome
+                    Welcome to VoKaB
                     
                                
                 </div>
                     <div class="panel-body">
-                            Welcome to VoKaBulary. Building this site is aimed at helping me to develop a great way to store new words that I look up in the dictionary. At the same time it fulfills the purpose of helping me to learn a new web framework.<br>
-                            You may search for Spanish/English translations and save them to lists for later reference. If you are just starting name a list first for the option to save your searches.
+                            <p>Building this site is aimed at helping me to develop a great way to store new words that I look up in the dictionary. While there are countless options online none of them place the initial search for the words translation at the front of the process. For me this feels more natural as the first thing I do when I don't know a word is open an online dictionary. At the same time it fulfills the purpose of helping me to learn a new web framework.<p>
+                            <p>You may search for Spanish/English translations and save them to lists for later reference. If you are just starting name a list first for the option to save your searches.</p>
+                            <a href="/home">Go to your dashboard to get started</a>
+                            
                           
                     </div>
                         
