3bec29e33fbcc83e852d45cb7c2f15337a6da933
randygan <randygannaway@gmail.com>
2016-06-04 08:55:20 -0500

    fixed study lists links in buttons

--- resources/views/lists/studyList.blade.php
+++ resources/views/lists/studyList.blade.php
@@ -22,8 +22,7 @@
 <div class="list-group">
             @foreach ($lists as $lists)   
                 <li class="list-group-item clearfix" id="word">
-                    <a href="/words/{{ $lists->list_id }}" type="submit" class="btn btn-primary" role="button">{{ $lists->list_name }}</a>
-                    <a href="{{ route('deleteList', ['list_id' => $lists->list_id ]) }}" class="pull-right"><span class="glyphicon glyphicon-trash" title="DELETE" onclick="return confirm('Delete the list {{ $lists->list_name }}?')"></span></a>
+                    <a href="{{ route('flashcards', ['list_id' => $lists->list_id ]) }}" type="submit" class="btn btn-primary" role="button">{{ $lists->list_name }}</a>
                 </li>
             
             @endforeach
